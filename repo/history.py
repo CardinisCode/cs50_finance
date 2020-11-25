217b76cd93581c003608b79a4183b68c1512ff37
Andi Folgado <ac.folgado@gmail.com>
2020-11-25 12:31:05 +0000

    Modified the getTransactionHistoryByUserID method to grab data by Date in Desc order

--- repo/history.py
+++ repo/history.py
@@ -3,7 +3,7 @@ class HistoryRepository:
         self.db = db
 
     def getTransactionHistoryByUserId(self, user_id):
-        return self.db.execute("SELECT * FROM history WHERE user_id = :user_id", user_id=user_id)
+        return self.db.execute("SELECT * FROM history WHERE user_id = :user_id ORDER BY date DESC", user_id=user_id)
 
     def InsertTransactionDetails(self, user_id, symbol, price, value, date, shares, trans_type):
         self.db.execute("INSERT INTO history (user_id, symbol, stock_price, purchase_value, date, shares, trans_type) VALUES (:user_id, :symbol, :price, :purchase_value, :date, :shares, :trans_type)", user_id=user_id, symbol=symbol, price=price, purchase_value=value, date=date, shares=shares, trans_type=trans_type)
